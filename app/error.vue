503b1851bacffd781f39f3fabffbbb0705170287
IT-Gleb <139981680+IT-Gleb@users.noreply.github.com>
2025-09-24 06:03:38 +0300

    error page added

---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <section class="my-10 w-fit mx-auto">
+    <h1 class="text-2xl font-bold text-error">{{ props.error?.statusCode }}</h1>
+    <p class="my-5 text-sm">Ошибка: {{ props.error?.message }}</p>
+    <NuxtLink to="/" class="my-10 underline underline-offset-2">
+      На Главную</NuxtLink
+    >
+  </section>
+</template>
